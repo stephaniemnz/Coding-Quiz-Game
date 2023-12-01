d5367140635a2a4e6cef670c20b1efed0da1e981
stephramos <stephaniemnz@gmail.com>
2023-11-30 22:12:37 -0600

    add question promts on js

---
+++ script.js
@@ -0,0 +1,45 @@
+let questions = [ 
+	{ 
+		prompt: 'The condition in an if / else statement is enclosed within ____.',
+		options: [ 
+			"quotes", 
+			"curly brackets", 
+			"parenthesis", 
+			"square brackets" 
+		], 
+		answer: "curly brackets",
+	}, 
+
+	{ 
+		prompt: `Arrays in JavaScript can be used to store _____.`, 
+		options: [ 
+			"numbers and strings",
+			"other arrays()", 
+			"boolean", 
+			"all the above"
+		], 
+		answer: "all the above", 
+	}, 
+
+	{ 
+		prompt: `String values must be enclosed within _____ when being assigned to variables.`, 
+		options: [ 
+			"commas", 
+			"curly brackets",
+			"quotes",
+			"parenthesis" 
+		], 
+		answer: "quotes", 
+	}, 
+
+	{ 
+		prompt: `A very useful tool used during development and debugging for printing content to the debugger is:`, 
+		options: [
+      "JavaScript",
+      "terminal/bash",
+      "for loops",
+      "console.log"
+    ], 
+		answer: "console.log", 
+	}, 
+]; 
